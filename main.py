6da0f1f1213c9f2aa63c9fab8b1ec64814d46d34
favero.santos <favero.santos@velsis.com.br>
2018-05-03 14:44:15 -0300

    Melhoria de código, limpeza e ajustes

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ class Defines:
 
     # Sampling frequency
     FS = 50000
+    # Number of samples
     N_SAMPLES = 10000
 
+# Generates a sine wave
 def generate_sine_wave(frequency, offset, amplitude):
     local_frequency = frequency
     DC = offset
@@ -32,7 +34,7 @@ def generate_PDF(gain, center_position, standard_deviation):
 
     return [x, y]
 
-def save_signal(name, x, y):
+def save_signal(name, y):
     file = open(name,"w")
     for local_index in range(0, len(y)):
         file.write(str(y[local_index]))
@@ -42,12 +44,12 @@ def save_signal(name, x, y):
 def main():
 
     #generate_sine_wave(1000, 1.65, 1.65)
-    plot_data = generate_PDF(1200, 5000, 600)
+    plot_data = generate_PDF(120000, 5000, 600)
 
     plt.plot(plot_data[0], plot_data[1])
     plt.show()
 
-    save_signal("gauss_pdf.txt", plot_data[0], plot_data[1])
+    save_signal("gauss_pdf_V00_R02.txt", plot_data[1])
 
 
 if __name__ == "__main__":
