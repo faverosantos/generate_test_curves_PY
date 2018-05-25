a0459e55c3ebc7bb8cd01ae8273b1d07936a82e6
favero.santos <favero.santos@velsis.com.br>
2018-05-25 09:13:59 -0300

    Code adjusts

--- README.md
+++ README.md
@@ -3,6 +3,10 @@ Generates some test curves using Python 3.0
 
 # How to convert number of samples to time?
 NS = SD*FS - 1 \
-SD being signal duration in {s} \
-FS being sampling frequency in (Hz) \
+SD being signal duration in [s] \
+FS being sampling frequency in [Hz] \
 NS being number of samples
+
+# Specs
+Made with Python 3.6.1 \
+Depends on: numpy 1.12.1 and matplotlib 2.0.2
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class Defines:
     # Sampling frequency
     FS = 50000
     # Number of samples
-    N_SAMPLES = 10000
+    N_SAMPLES = 20000
 
 # Generates a sine wave
 def generate_sine_wave(frequency, offset, amplitude):
@@ -32,7 +32,7 @@ def generate_PDF(gain, center_position, standard_deviation):
 
     y = a * np.exp(d)
 
-    return [x, y]
+    return y
 
 def save_file(name, y):
     file = open(name,"w")
@@ -41,15 +41,38 @@ def save_file(name, y):
         file.write("\n")
     file.close()
 
+def generate_exponential():
+    x = np.arange(Defines.N_SAMPLES)
+
+    Y1 = 3216.71869
+    Y2 = 1615.88683
+    R1 = -0.00058
+    R2 = -2.02E-04
+    A1 = -42031.31237
+    A2 = -3523.87996
+
+    T = 35.6
+
+    y1 = Y1 + A1*np.exp(R1*x)
+    y2 = T*(Y2 + A2*np.exp(R2*x))
+
+    return [y1, y2]
+
 def main():
 
     #generate_sine_wave(1000, 1.65, 1.65)
-    plot_data = generate_PDF(120000, 5000, 600)
+    #plot_data = generate_PDF(120000, 5000, 600)
+    [plot_data_1, plot_data_2] = generate_exponential()
 
-    plt.plot(plot_data[0], plot_data[1])
+    plt.plot(plot_data_1, label="P dependent only")
+    plt.plot(plot_data_2, label="T and P dependent")
+    plt.plot(plot_data_1 + plot_data_2, label="sum of both")
+    plt.legend(loc='upper right', bbox_to_anchor=(1, 1.1))
+    plt.ylim([0, 60000])
+    plt.xlim([0, 20000])
     plt.show()
 
-    save_file("gauss_pdf_V00_R02.txt", plot_data[1])
+    #save_file("gauss_pdf_V00_R02.txt", plot_data[1])
 
 
 if __name__ == "__main__":
